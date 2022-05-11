7d9f32d0d7a60cd005ea8a64768a2d640e74ae03
Young-Myung Choi <youngmyung.choi@pusan.ac.kr>
2022-05-11 17:20:59 +0900

    Testing the compilation in windows.
      * Intel Fortran has a fortran flag for math kernel library.
    
    Signed-off-by: Young-Myung Choi <youngmyung.choi@pusan.ac.kr>

--- config/findMathKernel.cmake
+++ config/findMathKernel.cmake
@@ -3,23 +3,34 @@ message("${ColourReset}")
 
 if ("${CMAKE_Fortran_COMPILER_ID}" MATCHES "Intel")
 
+    ## NOTE: Math Kernel Library with intel fortran ----------------------------
+    ##                                                                        ##
+    ## If the intel fortran is used, the math kernel library is already added ##
+    ## in the compilation flag.                                               ##
+    ##                                                                        ##
+    ## "/Qmkl:sequential" for windows                                         ##
+    ## "-qmkl:sequential" for linux                                           ##
+    ##                                                                        ##
+    ## -------------------------------------------------------------------------
+
     ## Find MKL Lib
-    set(MKL_LIB_CORRECT FALSE)
-    set(MKL_LIB_NAME "mkl_intel_lp64" "mkl_sequential" "mkl_core" "mkl_blas95_lp64" )
-    set(MKL_LIB_PATH_HINT
-        "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries/windows/mkl/lib/intel64"
-        "/opt/intel/compilers_and_libraries/linux/mkl/lib/intel64"
-        "/opt/intel/oneapi/mkl/latest/lib/intel64" )
-
-    message("${Yellow}Searching MKL Libraries... ${ColourReset}")
-    message("${ColourReset}")
-
-    ## Find Math Kernel Library first
-    FindLibraryAndGetFlag(
-        MKL_LIB_FLAGS
-        MKL_LIB_CORRECT
-        LIB_NAMES       "${MKL_LIB_NAME}"
-        LIB_PATH_HINT   "${MKL_LIB_PATH_HINT}" )
+    # set(MKL_LIB_CORRECT FALSE)
+    # set(MKL_LIB_NAME "mkl_intel_lp64" "mkl_sequential" "mkl_core" "mkl_blas95_lp64" )
+    # set(MKL_LIB_PATH_HINT
+    #     "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries/windows/mkl/lib/intel64"
+	# 	"C:/Program Files (x86)/Intel/oneAPI/mkl/latest/lib/intel64"
+    #     "/opt/intel/compilers_and_libraries/linux/mkl/lib/intel64"
+    #     "/opt/intel/oneapi/mkl/latest/lib/intel64" )
+    #
+    # message("${Yellow}Searching MKL Libraries... ${ColourReset}")
+    # message("${ColourReset}")
+    #
+    # ## Find Math Kernel Library first
+    # FindLibraryAndGetFlag(
+    #     MKL_LIB_FLAGS
+    #     MKL_LIB_CORRECT
+    #     LIB_NAMES       "${MKL_LIB_NAME}"
+    #     LIB_PATH_HINT   "${MKL_LIB_PATH_HINT}" )
 
 elseif("${CMAKE_Fortran_COMPILER_ID}" MATCHES "GNU")
 
